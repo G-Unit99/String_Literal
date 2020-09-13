10c315fccecb5217bd74c60db76418f0159a5f4e
gerry <gerry.scott99@gmail.com>
2020-09-13 02:48:41 -0400

    Started the project again after losing initial code.

---
+++ cmake-build-debug/CMakeFiles/String_Literal.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/String_Literal.dir/main.cpp.obj"
+  "String_Literal.exe"
+  "String_Literal.exe.manifest"
+  "String_Literal.pdb"
+  "libString_Literal.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/String_Literal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
