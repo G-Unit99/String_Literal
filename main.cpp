99bd3217d4b9a0b1576ba5c8f8da67f692d3c063
gerry <gerry.scott99@gmail.com>
2020-10-25 15:18:51 -0400

    Check_Error() function correctly checks decimals, terminating cases, exponents, and symbols.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
 #include <string>
-#include <cmath>
 
 using namespace std;
 void Is_literal(string);
-bool Check_exponent(string);
-bool Check_decimal(string);
-bool Check_symbol(string);
 bool Check_error(string);
 
 
 int main()
 {
-    bool a;
-    string test = ".12.3";
-    cout << a << endl;
+    string test = "3";
     cout << "Hello, World!" << endl;
     for (int i = 0; i < test.length();i++){
         cout << isdigit(test[i]) << endl;
@@ -28,31 +22,13 @@ void Is_literal(string number)
 {
     if (!Check_error(number))
     {
-        cout << " is NOT a valid numeric literal" << endl;
+        cout << number << " is NOT a valid numeric literal" << endl;
     }
 
     else
     {
-        cout << "is a valid numeric literal" << endl;
+        cout << number << " is a valid numeric literal" << endl;
     }
-}
-
-bool Check_exponent(string number)
-{
-    for (int i = 0; i < number.length();i++)
-    {
-
-    }
-}
-
-bool Check_decimal(string number)
-{
-
-}
-
-bool Check_symbol(string number)
-{
-
 }
 
 bool Check_error(string number)
@@ -61,37 +37,31 @@ bool Check_error(string number)
     const int max = 50;
     int decimal_count = 0;
     int length = number.length();
+//Check terminating cases that do not require iterating through the string//
 
+//Checks if string length is over 50 characters long//
     if (length > max)
     {
         cout << "false";
         return !literal;
     }
-
-
+//checks if program ends with an exponent symbol, starts with an exponent symbol, or ends with a unary symbol +/- //
+    if (number[length - 1] == 'e' || number[length -1] == 'E'|| number[length -1] == '+' || number[length- 1] == '-' || number[0] == 'e' || number[0] == 'E')
+    {
+        cout << "false 1";
+        return !literal;
+    }
 
     for (int i = 0; i < length; i++)
     {
-//check beginning inputs for errors, such as starting with an exponent or a symbol other than + or - or .//
-        if (i == 0)
+        cout << i << length<< endl;
+//check if program can detect an input that is not a number, an exponent, a decimal, or unary symbol +/- //
+        if (!isdigit(number[i]) && number[i] != 'e' && number[i] != 'E' && number[i] != '.' && number[i] != '+' && number[i] != '-')
         {
-            if (!isdigit(number[0]) && number[0] != '+' && !isdigit(number[0]) && number[0] != '-' && !isdigit(number[0]) && number[0] != '.')
-            {
-                cout << "false 1";
-                return !literal;
-            }
-            else if (number[0] == '.')
-            {
-                decimal_count += 1;
-            }
-        }
-//check if program can detect an input that is not a number, an exponent, or a decimal
-        else if (!isdigit(number[i]) && number[i] != 'e' && number[i] != 'E' && number[i] != '.')
-        {
-            cout<< "false 2";
+            cout << "false 2";
             return !literal;
         }
-//checks for decimals in the program
+//checks for decimals in the program//
         else if (number[i] == '.')
         {
             if (decimal_count > 0)
@@ -101,9 +71,37 @@ bool Check_error(string number)
             }
             else
             {
-
+                decimal_count += 1;
             }
         }
+//Checks for exponents//
+        else if (number[i] == 'e' || number[i] == 'E')
+        {
+//Check that an exponent symbol is not followed by another exponent symbol//
+            if (number[i + 1] == 'e' || number[i + 1] == 'E')
+            {
+                cout<< "false 4";
+                return !literal;
+            }
+            else if (number[i + 1] == '+' || number[i + 1] == '-')
+            {
+                if (!isdigit(number[i +2]))
+                {
+                    cout << "false 5";
+                    return !literal;
+                }
+            }
+        }
+//Check for +/- symbols//
+        else if (number[i] == '+' || number[i] == '-')
+        {
+            if (number[i +1] == '+' || number[i +1] == '-')
+            {
+                cout << "false 6";
+                return !literal;
+            }
+        }
+
     }
 
     cout <<" valid" << endl;
